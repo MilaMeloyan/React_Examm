e64816864fcab870ad02bb08d4ea1bd9e601aaa0
MilaMeloyan <milenameloyan726@gmail.com>
2023-02-25 15:54:00 +0400

    ...

---
+++ src/components/data.js
@@ -0,0 +1,41 @@
+const Data= {
+    products :[
+        {
+            id:'1',
+            name:'Mackbook',
+            price:700.000,
+            image:'https://picsum.photos/id/180/2400/1600',
+        },
+        {
+            id:'2',
+            name:'Phone',
+            price:300.000,
+            image:'https://picsum.photos/id/180/2400/1600',
+        },
+        {
+            id:'3',
+            name:'Air Pods',
+            price:50.000,
+            image:'https://picsum.photos/id/180/2400/1600',
+        },
+        {
+            id:'4',
+            name:'Samsung Galaxy S22 Ultra',
+            price:572.900,
+            image:'https://picsum.photos/id/180/2400/1600',
+        },
+        {
+            id:'5',
+            name:'Smart Watch',
+            price:30.900,
+            image:'https://picsum.photos/id/180/2400/1600',
+        },
+        {
+            id:'6',
+            name:'Bag',
+            price:10.000,
+            image:'https://picsum.photos/id/180/2400/1600',
+        }
+    ]
+}
+export default Data
